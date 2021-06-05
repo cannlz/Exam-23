3cfcc3cd173640b60ea87579b51f2573053c4a85
cannlz <52014546+cannlz@users.noreply.github.com>
2021-06-05 15:08:42 +0300

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/StockClass.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class StockClass
+    {
+        public int razm;
+        public int count;
+
+        public void Schet()
+        {
+            Console.Write("Введите размерность массива: ");
+            razm = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[razm];
+            Random rnd = new Random();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rnd.Next(-10,10);
+                Console.WriteLine(arr[i]);
+            }
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] >= 0 )
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine("Кол-во положителдьных чисел: " + count);
+            Console.ReadKey();
+        }
+    }
+}
